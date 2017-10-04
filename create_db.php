f6cdb2a0b9726cb822b406362340b56780c38b7e
alexeyvolkovoy <alexeyvolkovoy@gmail.com>
2017-10-04 17:04:41 +0300

    56

--- create_db.php
+++ create_db.php
@@ -5,11 +5,10 @@ $mydbconn->connect($dbserver,$dbuser,$dbpassword,$dbname);
 if ($mydbconn->connect_error) 
   echo "Error DB connect number ".$mydbconn->connect_errno; 
 else echo "Ser GUT!";
-$credb="CREATE TABLE SITES
-			ID  int(),
+$credb="CREATE TABLE IF NOT EXIST SITES (
+			ID  int(16) AUTO_INCREMENT PRIMARY KEY,
 			URL text(),
-			CREATED_BY_USER_ID int();
-			";
+			CREATED_BY_USER_ID int(16))";
 $mydbconn->real_query($credb);
 $mydbconn->commit();
 if ($mydbconn->error) 
