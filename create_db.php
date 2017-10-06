bfc97c86da1aa7b16cb1a32e81e72b7c049d8bec
alexeyvolkovoy <alexeyvolkovoy@gmail.com>
2017-10-06 15:11:30 +0300

    fyt

--- create_db.php
+++ create_db.php
@@ -5,12 +5,23 @@ $mydbconn->connect($dbserver,$dbuser,$dbpassword,$dbname);
 if ($mydbconn->connect_error) 
   echo "Error DB connect number ".$mydbconn->connect_errno; 
 else echo "Ser GUT!";
-$credb="CREATE TABLE IF NOT EXISTS SITES (
-			ID  int(16) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-			URL varchar(256) NOT NULL,
-			CREATED_BY_USER_ID int(16) NOT NULL)";
+$credb[]=(
+"DROP TABLE 'study'.'USERS'",
+"CREATE TABLE `study`.`USERS` ( `ID` INT(16) NOT NULL AUTO_INCREMENT PRIMARY KEY, `USER_NIC` VARCHAR(64) NOT NULL , `USRE_F_NAME` VARCHAR(64) NULL , `USER_L_NAME` VARCHAR(64) NULL , `PASSWORD` VARCHAR(1024) NOT NULL , `CELL_PHONE` VARCHAR(32) NOT NULL , `E_MAIL` VARCHAR(64) NOT NULL , `VIBER` VARCHAR(32) NOT NULL) ENGINE = InnoDB",
+"DROP TABLE 'study'.'SITES'",
+"CREATE TABLE `study`.`SITES` ( `ID` INT(16) NOT NULL AUTO_INCREMENT , `SITE_NAME` VARCHAR(256) NOT NULL , `URL` VARCHAR(256) NOT NULL , `COMMENT` VARCHAR(256) NULL , `ANSWER` TEXT NOT NULL , `REG_DT` DATETIME NOT NULL , `OWNER` INT(16) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB",
+"DROP TABLE 'study'.'STAT'",
+"CREATE TABLE `study`.`STAT` ( `ID` INT(16) NOT NULL AUTO_INCREMENT , `SITE_ID` INT(16) NOT NULL , `STATE_ID` INT(16) NOT NULL , `CHECK_DT` DATETIME NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB",
+"DROP TABLE 'study'.'STATES'",
+"CREATE TABLE `study`.`STATES` ( `ID` INT(16) NOT NULL AUTO_INCREMENT , `STATE` VARCHAR(64) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB"
+);
+
+
+
 $mydbconn->real_query($credb);
 $mydbconn->commit();
 if ($mydbconn->error) 
   echo "Error DB ".$mydbconn->errno; 
 ?>
+
+
