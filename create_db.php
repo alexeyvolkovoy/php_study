c198397c3cfc13e8cfe5022b9d5a3ced1f341b24
alexeyvolkovoy <alexeyvolkovoy@gmail.com>
2017-10-06 16:33:03 +0300

    5tty

--- create_db.php
+++ create_db.php
@@ -20,8 +20,9 @@ foreach($credb as $dbconnand) {
    $mydbconn->real_query($dbconnand);
    $mydbconn->commit();
 //   echo $mydbconn->connect_error."<br/>";
- //  if ($mydbconn->connect_error) 
- //    echo "Error DB ".$mydbconn->connect_errno); 
+   if ($mydbconn->connect_error) 
+     echo "Error DB ".$mydbconn->connect_errno);
+   else echo "SQL command run successfull<br/>";
 
 }
 
