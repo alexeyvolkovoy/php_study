72d76e026325d68c00582ee387960b44adbc687c
alexeyvolkovoy <alexeyvolkovoy@gmail.com>
2017-10-06 15:18:09 +0300

    erty

--- create_db.php
+++ create_db.php
@@ -5,7 +5,7 @@ $mydbconn->connect($dbserver,$dbuser,$dbpassword,$dbname);
 if ($mydbconn->connect_error) 
   echo "Error DB connect number ".$mydbconn->connect_errno; 
 else echo "Ser GUT!";
-$credb[]=(
+$credb=array(
 "DROP TABLE 'study'.'USERS'",
 "CREATE TABLE `study`.`USERS` ( `ID` INT(16) NOT NULL AUTO_INCREMENT PRIMARY KEY, `USER_NIC` VARCHAR(64) NOT NULL , `USRE_F_NAME` VARCHAR(64) NULL , `USER_L_NAME` VARCHAR(64) NULL , `PASSWORD` VARCHAR(1024) NOT NULL , `CELL_PHONE` VARCHAR(32) NOT NULL , `E_MAIL` VARCHAR(64) NOT NULL , `VIBER` VARCHAR(32) NOT NULL) ENGINE = InnoDB",
 "DROP TABLE 'study'.'SITES'",
@@ -15,13 +15,15 @@ $credb[]=(
 "DROP TABLE 'study'.'STATES'",
 "CREATE TABLE `study`.`STATES` ( `ID` INT(16) NOT NULL AUTO_INCREMENT , `STATE` VARCHAR(64) NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB"
 );
+foreach($credb as $dbconnand) {
+   $mydbconn->real_query($dbconnand);
+   $mydbconn->commit();
+   if ($mydbconn->error) 
+     echo "Error DB ".$mydbconn->errno; 
+
+}
 
 
-
-$mydbconn->real_query($credb);
-$mydbconn->commit();
-if ($mydbconn->error) 
-  echo "Error DB ".$mydbconn->errno; 
 ?>
 
 
